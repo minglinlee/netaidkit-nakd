22016f44e3b2979c561beb9c99cd383362b87381
minglin <minglin@alfa.com.tw>
2016-12-12 11:54:35 +0800

    1. Modify led for ALFA AP121F. 2. Change TOR/VPN connectivity indicator LED define.

--- root/usr/share/nakd/scripts/toggle_tor.sh
+++ root/usr/share/nakd/scripts/toggle_tor.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 /usr/share/nakd/scripts/reset_iptables.sh # iptables flushing
+LAN_LED_PATH="/sys/class/leds/alfa:yellow:lan/brightness"
+WLAN_LED_PATH="/sys/class/leds/alfa:yellow:wlan/brightness"
+SYS_LED_PATH="/sys/class/leds/alfa:yellow:sys/brightness"
 
 if [ $1 = "on" ];then
     rm /var/log/tor/notices.log
@@ -9,16 +12,18 @@ if [ $1 = "on" ];then
     uci set firewall.@redirect[2].enabled=1;
     uci set firewall.@forwarding[0].enabled=0;
     /usr/share/nakd/scripts/set_stage.sh 3
-    echo "1" > /sys/class/leds/gl-connect\:green\:lan/brightness
-    echo "0" > /sys/class/leds/gl-connect\:red\:wlan/brightness
+    #echo "1" > $LAN_LED_PATH
+    #echo "0" > $WLAN_LED_PATH
+	echo "1" > $SYS_LED_PATH
 elif [ $1 = "off" ]
 then
     /etc/init.d/tor stop
     uci set firewall.@redirect[1].enabled=0;
     uci set firewall.@redirect[2].enabled=0;
     /usr/share/nakd/scripts/set_stage.sh 2
-    echo "0" > /sys/class/leds/gl-connect\:green\:lan/brightness
-    echo "1" > /sys/class/leds/gl-connect\:red\:wlan/brightness
+    #echo "0" > $LAN_LED_PATH
+    #echo "1" > $WLAN_LED_PATH
+	echo "0" > $SYS_LED_PATH
 fi
 
 uci commit;
--- root/usr/share/nakd/scripts/toggle_vpn.sh
+++ root/usr/share/nakd/scripts/toggle_vpn.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 /usr/share/nakd/scripts/reset_iptables.sh # iptables flushing
+LAN_LED_PATH="/sys/class/leds/alfa:yellow:lan/brightness"
+WLAN_LED_PATH="/sys/class/leds/alfa:yellow:wlan/brightness"
+SYS_LED_PATH="/sys/class/leds/alfa:yellow:sys/brightness"
 
 if [ $1 = "on" ];then
 
@@ -13,8 +16,9 @@ if [ $1 = "on" ];then
     openvpn --log-append /var/log/openvpn.log --daemon --config /nak/ovpn/current.ovpn
 
     /usr/share/nakd/scripts/set_stage.sh 4
-    echo "1" > /sys/class/leds/gl-connect\:green\:lan/brightness
-    echo "0" > /sys/class/leds/gl-connect\:red\:wlan/brightness
+    #echo "1" > $LAN_LED_PATH
+    #echo "0" > $WLAN_LED_PATH
+	echo "1" > $SYS_LED_PATH
 elif [ $1 = "off" ]
 then
 
@@ -25,8 +29,9 @@ then
     killall -9 openvpn
 
     /usr/share/nakd/scripts/set_stage.sh 2
-    echo "0" > /sys/class/leds/gl-connect\:green\:lan/brightness
-    echo "1" > /sys/class/leds/gl-connect\:red\:wlan/brightness
+    #echo "0" > $LAN_LED_PATH
+    #echo "1" > $WLAN_LED_PATH
+	echo "0" > $SYS_LED_PATH
 fi
 
 /etc/init.d/firewall restart
